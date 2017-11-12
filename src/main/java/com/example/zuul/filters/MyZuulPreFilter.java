72f8a5b2245cc29ede1894ac5daa9b68803933a0
ravchappa22 <chapparavi22@gmail.com>
2017-11-11 20:27:30 -0500

    fixed the response from auth server to refer to defaultOauth token

--- src/main/java/com/example/zuul/filters/MyZuulPreFilter.java
+++ src/main/java/com/example/zuul/filters/MyZuulPreFilter.java
@@ -3,13 +3,12 @@ package com.example.zuul.filters;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -35,30 +34,28 @@ public class MyZuulPreFilter extends ZuulFilter {
             ctx.setResponseBody("static content"); 
             //ctx.setSendZuulResponse(true); 
         } 
-             
-
+        
 		String smUser = ctx.getRequest().getHeader("SM_USER");
 		String smUserGroups = ctx.getRequest().getHeader("SM_USERGROUPS");
 		if(smUser == null || smUserGroups == null){
 			ctx.setSendZuulResponse(false);
 		}else{
-			String token = null;
-			//tokenAuthClient.userToken(smUserGroups, smUser);
-			//restTemplate.getForObject("http://localhost:9090/getTokenForUser", String.class, new String[]{smUserGroups,smUser});
 			HttpHeaders httpHeaders = new HttpHeaders();
 			httpHeaders.add("roles", smUserGroups);
 			httpHeaders.add("userName", smUser);
 			HttpEntity <String> httpEntity = new HttpEntity <String> (null, httpHeaders);
-			ResponseEntity<String> response = null;
+			ResponseEntity<DefaultOAuth2AccessToken> response = null;
 	        try{
-			    response = restTemplate.exchange("http://localhost:8080/auth/getTokenForUser", HttpMethod.GET, httpEntity, String.class);
+			    response = restTemplate.exchange("http://localhost:8080/auth/getTokenForUser", HttpMethod.GET, httpEntity, DefaultOAuth2AccessToken.class);
 	        }catch(Exception e){
 	        	e.printStackTrace();
 	        }
 	        
 			System.out.println("Token we got>>>= " + response);
 	        Map<String, String> map = new HashMap<String, String>();
-			map.put("Authorization", "bearer "+response.getBody());
+	        System.out.println("body"+ response.getBody());
+			map.put("Authorization", response.getBody().getTokenType()+" "+response.getBody().getValue());
+	       
 			map.put("SM_USER", smUser);
 			map.put("SM_USERGROUPS", smUserGroups);
 			ctx.getZuulRequestHeaders().putAll(map);
@@ -69,7 +66,6 @@ public class MyZuulPreFilter extends ZuulFilter {
 
 	@Override
 	public boolean shouldFilter() {
-		
 		return true;
 	}
 
